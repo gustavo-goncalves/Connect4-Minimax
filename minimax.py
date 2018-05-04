514d6297263ba527b8c19a989606736d5e5e1fd3
gabrielcs23 <gabrielcs23@hotmail.com>
2018-05-04 19:38:24 -0300

    adição da poda

--- minimax.py
+++ minimax.py
@@ -9,7 +9,7 @@ class Int_Art:
     def __init__(self, jogo_atual):
         self.jogo_copia = copy.deepcopy(jogo_atual)
 
-    def max_retorno(self, contador):
+    def max_retorno(self, contador, alpha, beta):
         if self.jogo_copia.alguemGanhou() != 0 or self.jogo_copia.cheio() or contador == PROFUNDIDADEM:
             return pontuacao(self.jogo_copia.tabuleiro)
         valor_no = float('-inf')
@@ -17,11 +17,14 @@ class Int_Art:
             if i == -1:
                 continue
             self.jogo_copia.coloca_disco(i)
-            valor_no = max(valor_no, self.mini_retorno(contador+1))
+            valor_no = max(valor_no, self.mini_retorno(contador+1, alpha, beta))
+            if valor_no >= beta:
+                return valor_no
+            alpha = max(alpha, valor_no)
             self.jogo_copia.retira_disco(i)
         return valor_no
 
-    def mini_retorno(self, contador):
+    def mini_retorno(self, contador, alpha, beta):
         if self.jogo_copia.alguemGanhou() != 0 or self.jogo_copia.cheio() or contador == PROFUNDIDADEM:
             return pontuacao(self.jogo_copia.tabuleiro)
         valor_no = float('inf')
@@ -29,10 +32,14 @@ class Int_Art:
             if i == -1:
                 continue
             self.jogo_copia.coloca_disco(i)
-            valor_no = min(valor_no, self.max_retorno(contador+1))
+            valor_no = min(valor_no, self.max_retorno(contador+1, alpha, beta))
+            if valor_no <= alpha:
+                return valor_no
+            beta = min(beta,valor_no)
             self.jogo_copia.retira_disco(i)
         return valor_no
 
+    """
     def minimax_retorno(self):
         profundidade = 0
         melhor_jogada = 0
@@ -42,6 +49,21 @@ class Int_Art:
                 continue
             self.jogo_copia.coloca_disco(i)
             valor_filho = self.mini_retorno(profundidade+1)
+            if valor_filho > valor_no:
+                melhor_jogada = i
+                valor_no = valor_filho
+            self.jogo_copia.retira_disco(i)
+        return melhor_jogada
+    """
+    def alpha_beta(self):
+        profundidade = 0
+        melhor_jogada = 0
+        valor_no = float('-inf')
+        for i in range(7):
+            if self.jogo_copia.ondeLivre[i] == -1:
+                continue
+            self.jogo_copia.coloca_disco(i)
+            valor_filho = self.max_retorno(profundidade+1, float('-inf'), float('inf'))
             if valor_filho > valor_no:
                 melhor_jogada = i
                 valor_no = valor_filho
